2285ef9e72cb67331bacb8aa45e135a26872dd10
Idabagus Indra <idabgusindra01@gmail.com>
2022-11-02 21:06:55 +0800

    add file constant.go

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string  = "bagus"
+		lastName          = "juara"
+		phi       float32 = 3.14
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(phi)
+}
