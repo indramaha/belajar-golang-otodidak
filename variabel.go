803eb84f560f5fc091eecda14ac4f82edcd9deec
Idabagus Indra <idabgusindra01@gmail.com>
2022-11-02 20:56:25 +0800

    add file variabel.go

---
+++ variabel.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "bagus"
+	fmt.Println(name)
+
+	var friendName = "mahamaha"
+	fmt.Println(friendName)
+
+	var age = 12
+	fmt.Println(age)
+
+	negara := "amerika"
+	fmt.Println(negara)
+
+	var (
+		firstName = "bagus"
+		lastName  = "belajar"
+	)
+	fmt.Println(firstName, lastName)
+}
