0f51331448fbb9ae1c28e4cdfd30142c3ac50042
Idabagus Indra <idabgusindra01@gmail.com>
2022-11-02 20:30:21 +0800

    add file string.go

---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("bagus")
+	fmt.Println("hai, bagus")
+	fmt.Println("welcome!")
+	fmt.Println(len("hai, bagus"))
+	fmt.Println("bagus"[1])
+}
