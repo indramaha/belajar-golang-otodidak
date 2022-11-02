11910f419fd964065f2acad934d7ed21a67db6b3
Idabagus Indra <idabgusindra01@gmail.com>
2022-11-02 21:23:45 +0800

    add file type-declaration.go

---
+++ type-declaration.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	type namaSaya string
+	type iniPecahan float32
+
+	var firstName namaSaya = "Bagus"
+	var noHp iniPecahan = 14.022
+	fmt.Println(firstName)
+	fmt.Println(noHp)
+}
